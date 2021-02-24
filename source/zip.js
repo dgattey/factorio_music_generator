c5118af771726eddabbdf3d20a6a765a4e397f28
Dylan Gattey <hi@dylangattey.com>
2021-02-23 22:52:18 -0800

    fix: formats all source files

--- source/zip.js
+++ source/zip.js
@@ -1,27 +1,29 @@
-const fs   = require('fs')
+const fs = require('fs')
 const path = require('path')
 const Zip = require('adm-zip')
 const { progress } = require('./log')
 
 // Async function that runs the zipping for a given folder. Factorio expects the modVersion to be appended
 const zipMod = async (destination, modVersion, folderName) => {
-    const folderPath = path.resolve(destination, folderName)
-    const zip = new Zip()
-    const zipName = `${folderName}_${modVersion}.zip`
-    await zip.addLocalFolderPromise(folderPath, folderName)
-    await zip.writeZipPromise(`${destination}/${zipName}`)
-    await fs.promises.rmdir(folderPath, { recursive: true })
+  const folderPath = path.resolve(destination, folderName)
+  const zip = new Zip()
+  const zipName = `${folderName}_${modVersion}.zip`
+  await zip.addLocalFolderPromise(folderPath, folderName)
+  await zip.writeZipPromise(`${destination}/${zipName}`)
+  await fs.promises.rmdir(folderPath, { recursive: true })
 }
 
 // Zips each mod folder, then deletes each mod folder. Called as a second pass
 const zipMods = async ({ destination, modVersion }) => {
-    const possibleFolders = await fs.promises.readdir(destination, { withFileTypes: true })
-    const promises = possibleFolders
-        .filter((folder) => folder.isDirectory())
-        .map((folder) => zipMod(destination, modVersion, folder.name))
-    await progress(Promise.all(promises), `Zip folders into mods`)
+  const possibleFolders = await fs.promises.readdir(destination, {
+    withFileTypes: true,
+  })
+  const promises = possibleFolders
+    .filter((folder) => folder.isDirectory())
+    .map((folder) => zipMod(destination, modVersion, folder.name))
+  await progress(Promise.all(promises), `Zip folders into mods`)
 }
 
 module.exports = {
-    zipMods
+  zipMods,
 }
